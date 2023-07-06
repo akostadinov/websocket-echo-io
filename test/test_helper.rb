4a837288c830681014087413c667a3fb91bb7704
Aleksandar N. Kostadinov <akostadi@redhat.com>
2023-07-07 02:10:42 +0300

    fix coverage properly.. or not

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,10 @@ $LOAD_PATH.unshift("#{__dir__}/lib")
 $LOAD_PATH.unshift("#{File.dirname(__dir__)}/lib")
 
 unless ENV["DISABLE_COVERAGE"] == "1"
+  # `bundle exec` loads gemspec and it in turn requires `version.rb` before
+  # simplecov is required. This prevents simplecov to cover `version.rb`.
+  $LOADED_FEATURES.reject! { |file| file.end_with? "websocket/echo/io/version.rb" }
+
   require "simplecov"
   SimpleCov.start do
     enable_coverage :branch
